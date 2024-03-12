ad4c309f39461b7c4bc0321ebe4af8ee5d03ded0
VaxlPluh <ilja.alexander@icloud.com>
2024-03-12 21:50:41 +0100

    MainMenu1

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 func _process(delta):
 	$Camera2D.global_position.x = $Player.position.x
 	$Camera2D.global_position.y = $Player.position.y -100
--- UI/UI_scripts/Menu.gd
+++ UI/UI_scripts/Menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 func _on_play_pressed():
+	get_node("/root/Ui").visible = true
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
-
+	
+	
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://UI/UI_tscn/options_menu.tscn")
-
+	
 func _on_quit_pressed():
 	get_tree().quit()
